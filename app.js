f2f6f70ff09ea4b88972edee5b128eb4c8f3b591
Henry-0810 <ml2002study@gmail.com>
2023-10-02 16:26:02 +0100

    favicon problem

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-var indexRouter = require("./app_server/routes/index");
-var usersRouter = require("./app_server/routes/users");
+const indexRouter = require("./app_server/routes/index");
+const usersRouter = require("./app_server/routes/users");
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "./app_server/views"));
 app.set("view engine", "pug");
 
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
