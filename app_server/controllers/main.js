e211145df040983a3298a5dcb91a6b20f9961efd
Henry-0810 <Henry.pan@students.ittralee.ie>
2023-10-23 17:56:44 +0100

    left controller and mongoose

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,35 @@
 /* GET home page */
+
+const events = [
+  {
+    title: "Basketball Competition",
+    description: "Versus the Lakers",
+    start: "2023-10-24T16:00:00",
+    end: "2023-10-24T18:00:00",
+  },
+  {
+    title: "Halloween Preparation",
+    description: "Party preparation for Halloween",
+    start: "2023-10-28",
+    end: "2023-10-30",
+  },
+  {
+    title: "Report Submission",
+    description: "Report submission for algorithms",
+    start: "2023-10-11T23:00:00",
+  },
+  {
+    title: "Coffee with John",
+    start: "2023-11-12T10:30:00",
+  },
+  {
+    title: "Lunch with family",
+    start: "2023-11-17T13:00:00",
+  },
+];
+
 const home = function (req, res) {
-  res.render("home", { title: "Sync Up" });
+  res.render("home", { title: "Sync Up", events });
 };
 
 module.exports = {
--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   let calendarEl = document.getElementById("calendar");
   let calendar = new FullCalendar.Calendar(calendarEl, {
+    customButtons: {
+      addEventButton: {
+        text: "add event",
+        click: function () {
+          console.log("Add Event button clicked!");
+        },
+      },
+    },
     headerToolbar: {
-      left: "prev,next today",
+      left: "prev,next today addEventButton",
       center: "title",
       right: "dayGridMonth,timeGridWeek,timeGridDay",
     },
@@ -10,13 +18,35 @@ document.addEventListener("DOMContentLoaded", function () {
     editable: true,
     events: [
       {
-        eventId: 1,
         title: "Basketball Competition",
         description: "Versus the Lakers",
         start: "2023-10-24T16:00:00",
         end: "2023-10-24T18:00:00",
       },
+      {
+        title: "Halloween Preparation",
+        description: "Party preparation for Halloween",
+        start: "2023-10-28",
+        end: "2023-10-30",
+      },
+      {
+        title: "Report Submission",
+        description: "Report submission for algorithms",
+        start: "2023-10-11T23:00:00",
+      },
+      {
+        title: "Coffee with John",
+        start: "2023-11-12T10:30:00",
+      },
+      {
+        title: "Lunch with family",
+        start: "2023-11-17T13:00:00",
+      },
     ],
+    // customize event style
+    eventTextColor: "white",
+    eventBackgroundColor: "#d6711e",
+    eventBorderColor: "#d6711e",
   });
   calendar.render();
   console.log("calendar.js loaded");
